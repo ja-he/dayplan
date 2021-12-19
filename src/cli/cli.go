c7cf572012373c294fdab40812fb32bd761b2ad1
Jan Hensel <ja_he@uni-bremen.de>
2021-12-19 22:13:36 +0100

    Implement `summarize` subcommand
    Alternatively to this I was thinking about having multiple commands
    as separate binaries in `cmd`. I tried this and it worked alright,
    but as `go-flags` provides this subcommand mechanism I think this is
    cleaner and less effort for further commands (although I had some
    trouble with it, lacking documentation to understand it fully).

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -2,6 +2,8 @@ package cli
 
 type CommandLineOpts struct {
 	Day string `short:"d" long:"day" description:"Specify the day to plan" value-name:"<file>"`
+
+	SummarizeCommand SummarizeCommand `command:"summarize" subcommands-optional:"true"`
 }
 
 var Opts CommandLineOpts
