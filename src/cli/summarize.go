51f5f69fd67039a970ddfbc2aac2816ded705701
Jan Hensel <ja_he@uni-bremen.de>
2021-12-19 22:13:36 +0100

    Implement `summarize` subcommand
    Alternatively to this I was thinking about having multiple commands
    as separate binaries in `cmd`. I tried this and it worked alright,
    but as `go-flags` provides this subcommand mechanism I think this is
    cleaner and less effort for further commands (although I had some
    trouble with it, lacking documentation to understand it fully).

---
+++ src/cli/summarize.go
@@ -0,0 +1,99 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ja-he/dayplan/src/filehandling"
+	"github.com/ja-he/dayplan/src/model"
+	"github.com/ja-he/dayplan/src/program"
+	"github.com/ja-he/dayplan/src/util"
+)
+
+type SummarizeCommand struct {
+	FromDay string `short:"f" long:"from" description:"the day from which to start summarizing" value-name:"<yyyy-mm-dd>" required:"true"`
+	TilDay  string `short:"t" long:"til" description:"the day til which to summarize (inclusive)" value-name:"<yyyy-mm-dd>" required:"true"`
+
+	HumanReadable        bool   `long:"human-readable" description:"format times as hours and minutes"`
+	CategoryFilterString string `long:"category-filter" description:"a filter for categories; any named categories included; all included if omitted" value-name:"<cat1>,<cat2>,..."`
+
+	Verbose bool `short:"v" long:"verbose" description:"provide verbose output"`
+}
+
+func (command *SummarizeCommand) Execute(args []string) error {
+	summarize()
+	return nil
+}
+
+func summarize() {
+	var programData program.Data
+
+	// set up dir per option
+	dayplanHome := os.Getenv("DAYPLAN_HOME")
+	if dayplanHome == "" {
+		programData.BaseDirPath = os.Getenv("HOME") + "/.config/dayplan"
+	} else {
+		programData.BaseDirPath = strings.TrimRight(dayplanHome, "/")
+	}
+
+	currentDate, err := model.FromString(Opts.SummarizeCommand.FromDay)
+	if err != nil {
+		panic(err)
+	}
+	finalDate, err := model.FromString(Opts.SummarizeCommand.TilDay)
+
+	filterCategories := len(Opts.SummarizeCommand.CategoryFilterString) > 0
+	includeCategoriesByName := make(map[string]struct{})
+	if filterCategories {
+		for _, name := range strings.Split(Opts.SummarizeCommand.CategoryFilterString, ",") {
+			includeCategoriesByName[name] = struct{}{}
+		}
+	}
+
+	// TODO: can probably make this mostly async?
+	days := make([]model.Day, 0)
+	for currentDate != finalDate.Next() {
+		fh := filehandling.NewFileHandler(programData.BaseDirPath + "/days/" + currentDate.ToString())
+		days = append(days, *fh.Read())
+
+		currentDate = currentDate.Next()
+	}
+
+	totalSummary := make(map[model.Category]int)
+	for _, day := range days {
+		daySummary := day.SumUpByCategory()
+		for category, duration := range daySummary {
+			totalSummary[category] += duration
+		}
+	}
+
+	if Opts.SummarizeCommand.Verbose {
+		fmt.Println("dayplan time summary:")
+
+		fmt.Println("from:            ", Opts.SummarizeCommand.FromDay)
+		fmt.Println("til:             ", Opts.SummarizeCommand.TilDay)
+		fmt.Println("category filter: ", Opts.SummarizeCommand.CategoryFilterString)
+
+		fmt.Println("read", len(days), "days")
+		fmt.Println("total summary:")
+	}
+
+	for category, duration := range totalSummary {
+		_, categoryIncluded := includeCategoriesByName[category.Name]
+		if filterCategories && !categoryIncluded {
+			continue
+		}
+
+		var durationStr string
+		if Opts.SummarizeCommand.HumanReadable {
+			durationStr = fmt.Sprint(util.DurationToString(duration))
+		} else {
+			durationStr = fmt.Sprint(duration, " min")
+		}
+		fmt.Print("  ")
+		fmt.Printf("% 20s : % 10s\n", category.Name, durationStr)
+	}
+
+	os.Exit(0)
+}
