7d67910d25284b3010a566247de1a63dcabcc69f
Jan Hensel <ja_he@uni-bremen.de>
2021-12-20 21:33:50 +0100

    Allow color parsing to deal with '#'
    just shuffling todos and unhandled errors around really...

--- src/colors/colors_test.go
+++ src/colors/colors_test.go
@@ -48,6 +48,47 @@ func TestColorFromHexString(t *testing.T) {
 			log.Fatalf("colors testcase '%s' failed: 0x%06x instead of 0x%06x", testcase, result.Hex(), expected.Hex())
 		}
 	}
+
+	{
+		testcase := "pure red"
+
+		input := "#ff0000"
+		expected := tcell.NewHexColor(0xff0000)
+		result := ColorFromHexString(input)
+		if result != expected {
+			log.Fatalf("colors testcase '%s' failed: 0x%06x instead of 0x%06x", testcase, result.Hex(), expected.Hex())
+		}
+	}
+	{
+		testcase := "pure green"
+
+		input := "#00ff00"
+		expected := tcell.NewHexColor(0x00ff00)
+		result := ColorFromHexString(input)
+		if result != expected {
+			log.Fatalf("colors testcase '%s' failed: 0x%06x instead of 0x%06x", testcase, result.Hex(), expected.Hex())
+		}
+	}
+	{
+		testcase := "pure blue"
+
+		input := "#0000ff"
+		expected := tcell.NewHexColor(0x0000ff)
+		result := ColorFromHexString(input)
+		if result != expected {
+			log.Fatalf("colors testcase '%s' failed: 0x%06x instead of 0x%06x", testcase, result.Hex(), expected.Hex())
+		}
+	}
+	{
+		testcase := "regular color"
+
+		input := "#123456"
+		expected := tcell.NewHexColor(0x123456)
+		result := ColorFromHexString(input)
+		if result != expected {
+			log.Fatalf("colors testcase '%s' failed: 0x%06x instead of 0x%06x", testcase, result.Hex(), expected.Hex())
+		}
+	}
 }
 
 func TestDarken(t *testing.T) {
