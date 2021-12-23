f1f328a9d28ac794aee5ed606509d95235808fda
Jan Hensel <ja_he@uni-bremen.de>
2021-12-23 11:33:17 +0100

    Write a string truncation function
    Will certainly be useful in a couple places in view

--- src/util/util.go
+++ src/util/util.go
@@ -8,3 +8,13 @@ func (r Rect) Contains(x, y int) bool {
 	return (x >= r.X) && (x < r.X+r.W) &&
 		(y >= r.Y) && (y < r.Y+r.H)
 }
+
+// TODO: test and use in appropriate places
+func TruncateAt(s string, length int) string {
+	r := []rune(s)
+	if len(r) <= length {
+		return s
+	} else {
+		return string(append(r[:length-3], []rune("...")...))
+	}
+}
