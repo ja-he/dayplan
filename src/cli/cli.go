3c5167358ae2e4998bc74040f1711166fb9c940f
Jan Hensel <ja_he@uni-bremen.de>
2021-12-26 20:16:08 +0100

    Add version subcommand and `-v`/`--version` flags

--- src/cli/cli.go
+++ src/cli/cli.go
@@ -1,9 +1,11 @@
 package cli
 
 type CommandLineOpts struct {
-	Day string `short:"d" long:"day" description:"Specify the day to plan" value-name:"<file>"`
+	Day     string `short:"d" long:"day" description:"Specify the day to plan" value-name:"<file>"`
+	Version bool   `short:"v" long:"version" description:"Show the program version"`
 
 	SummarizeCommand SummarizeCommand `command:"summarize" subcommands-optional:"true"`
+	VersionCommand   VersionCommand   `command:"version" subcommands-optional:"true"`
 }
 
 var Opts CommandLineOpts
