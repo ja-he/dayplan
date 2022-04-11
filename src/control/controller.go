22706661d4defc5ecf91d924d89bdeaa58719530
Jan Hensel <ja_he@uni-bremen.de>
2022-04-11 17:33:10 +0200

    Add comment priming `EditState` for deprecation

--- src/control/controller.go
+++ src/control/controller.go
@@ -70,6 +70,17 @@ type Controller struct {
 	syncer            tui.ScreenSynchronizer
 }
 
+// TODO(ja-he):
+//   Remove / Change to `MouseEditState` or similar.
+//   This type would allow the controller to track state of editing, which
+//   could be the editor overlay being active, using the mouse to resize an
+//   event, resizing a "selected" event with the KB, etc.
+//   However, I came up with it before implementing the fleshed-out keyboard
+//   input concept, and I prefer using the modal overlay concept. E. g. with a
+//   move, we would (instead of using the `EditState` type) add an input tree as
+//   an overlay to the focussed pane, which would have mappings for 'j' to move
+//   down and 'k' to move up (instead of having to catch the edit state edge
+//   case before regular input processing).
 type EditState int
 
 const (
