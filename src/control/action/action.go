877f2f04caf25a1d23adb1bf0e3f4570f13398e3
Jan Hensel <ja_he@uni-bremen.de>
2022-05-27 15:18:51 +0200

    Document control/action

--- src/control/action/action.go
+++ src/control/action/action.go
@@ -1,5 +1,12 @@
 package action
 
+// Action models a "doable" and potentially "undoable" action.
+//
+// If an action can be undone, its Undoable member is to return true, otherwise
+// false. Iff Undoable returns false, Undo should be a no-op.
+//
+// Actions can also -- e.g. for the purposes of help generation -- be described
+// by Explain. Explain is to return a string describing the action Do would perform.
 type Action interface {
 	Do()
 
--- src/control/action/simple.go
+++ src/control/action/simple.go
@@ -1,23 +1,34 @@
 package action
 
+// Simple implements the Action interface.
+// It models a simple, non-undoable action as a func() which is called on Do.
 type Simple struct {
-	action func()
-
+	action  func()
 	explain func() string
 }
 
+// Do performs this simple action.
+// A simple action is not undoable.
 func (a *Simple) Do() {
 	a.action()
 }
 
+// Undoable always returns false.
+// A simple action is not undoable.
 func (a *Simple) Undoable() bool { return false }
 
+// Undo does nothing.
+// A simple action is not undoable.
 func (a *Simple) Undo() {}
 
+// Explain returns the explanation for this simple action's Do member.
 func (a *Simple) Explain() string {
 	return a.explain()
 }
 
+// NewSimple returns a pointer to a new simple action, which stores the given
+// action function and the given explainer to use when prompted with Do or
+// Explain respectively.
 func NewSimple(explainer func() string, action func()) *Simple {
 	return &Simple{
 		action:  action,
