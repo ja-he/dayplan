98de4e2fc8bd1f147cbfafda3b122c0b8e6c9697
Jan Hensel <ja_he@uni-bremen.de>
2022-06-24 17:24:24 +0200

    feat: Implement RangedGoal

--- src/model/goal.go
+++ src/model/goal.go
@@ -7,3 +7,17 @@ import (
 type Goal interface {
 	Requires(Date) time.Duration
 }
+
+type RangedGoal struct {
+	Start Date
+	End   Date
+	Time  time.Duration
+}
+
+func (g *RangedGoal) Requires(date Date) time.Duration {
+	if date.IsBefore(g.Start) || date.IsAfter(g.End) {
+		return 0
+	} else {
+		return g.Time / time.Duration(g.Start.DaysUntil(g.End)+1)
+	}
+}
