df38f5a070af65644478f1623396c17392e1f8e6
Jan Hensel <ja_he@uni-bremen.de>
2022-06-24 17:24:46 +0200

    feat: Implement WorkweekGoal

--- src/model/goal.go
+++ src/model/goal.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -19,5 +20,36 @@ func (g *RangedGoal) Requires(date Date) time.Duration {
 		return 0
 	} else {
 		return g.Time / time.Duration(g.Start.DaysUntil(g.End)+1)
+	}
+}
+
+type WorkweekGoal struct {
+	Monday    time.Duration
+	Tuesday   time.Duration
+	Wednesday time.Duration
+	Thursday  time.Duration
+	Friday    time.Duration
+	Saturday  time.Duration
+	Sunday    time.Duration
+}
+
+func (g *WorkweekGoal) Requires(date Date) time.Duration {
+	switch date.ToWeekday() {
+	case time.Monday:
+		return g.Monday
+	case time.Tuesday:
+		return g.Tuesday
+	case time.Wednesday:
+		return g.Wednesday
+	case time.Thursday:
+		return g.Thursday
+	case time.Friday:
+		return g.Friday
+	case time.Saturday:
+		return g.Saturday
+	case time.Sunday:
+		return g.Sunday
+	default:
+		panic(fmt.Sprintf("unknown weekday %d", date.ToWeekday()))
 	}
 }
