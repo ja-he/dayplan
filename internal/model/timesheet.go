d60a7ae9843bcc64b93aae181065625b306dbbf8
Jan Hensel <ja_he@uni-bremen.de>
2023-01-05 14:20:38 +0100

    feat(timesheet): Allow getting timesheet entry per day per category

---
+++ internal/model/timesheet.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+// A TimesheetEntry represents an entry in a common timesheet.
+//
+// It defines a beginning (i.e. the time you clocked in), an end (i.e. the time
+// you clocked out), and the total length of breaks taken between them.
+type TimesheetEntry struct {
+	Start         Timestamp
+	BreakDuration time.Duration
+	End           Timestamp
+}
+
+// ToPrintableFormat returns this TimesheetEntry in its printable (CSV) format.
+func (e *TimesheetEntry) ToPrintableFormat() string {
+	return fmt.Sprintf(
+		"%s,%s,%s",
+		e.Start.ToString(),
+		e.BreakDuration.String(),
+		e.End.ToString(),
+	)
+}
