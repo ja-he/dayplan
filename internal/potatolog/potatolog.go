c28c446801e8ec89f085a056a952b0e9ce9ac74e
Jan Hensel <ceo@dubious.engineering>
2024-07-02 15:20:05 +0200

    style: slightly improve the memory logger

--- internal/potatolog/potatolog.go
+++ internal/potatolog/potatolog.go
@@ -6,23 +6,27 @@ import (
 	"sync"
 )
 
+// LogEntry is a single log entry.
 type LogEntry = map[string]string
 
+// GlobalMemoryLogReaderWriter is a global MemoryLogReaderWriter.
 var GlobalMemoryLogReaderWriter = MemoryLogReaderWriter{
 	mtx: sync.Mutex{},
 	log: []LogEntry{},
 }
 
+// MemoryLogReaderWriter is a simple in-memory log reader and writer.
 type MemoryLogReaderWriter struct {
 	mtx sync.Mutex
 	log []LogEntry
 }
 
+// Write appends a log entry to the log.
 func (w *MemoryLogReaderWriter) Write(p []byte) (int, error) {
 	entry := LogEntry{}
 	err := json.Unmarshal(p, &entry)
 	if err != nil {
-		return 0, fmt.Errorf("could not unmarshal log entry (%s)", err.Error())
+		return 0, fmt.Errorf("could not unmarshal log entry (err:%s) (input:'%s')", err.Error(), string(p))
 	}
 
 	w.mtx.Lock()
@@ -31,6 +35,7 @@ func (w *MemoryLogReaderWriter) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+// Get returns the log.
 func (w *MemoryLogReaderWriter) Get() []LogEntry {
 	return w.log
 }
