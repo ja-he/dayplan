c09c97050e8827c1148b1ca80de9a2f4e3c67640
Jan Hensel <ceo@dubious.engineering>
2024-07-10 12:31:18 +0200

    add commit fn and notes to iface

--- internal/storage/provider.go
+++ internal/storage/provider.go
@@ -8,6 +8,14 @@ import (
 
 // DataProvider is the abstracted data provider, which can be implemented over
 // various storage systems.
+//
+// The data provider's responsibility are as follows:
+//   - track events
+//   - provide useful queries over events
+//   - handle whatever backend backs the storage
+//     I think my idea here is that the creation of the concrete povider will
+//     potentially configure the desired behaviour of automatically storing
+//     changes to the backend or waiting for commits.
 type DataProvider interface {
 	AddEvent(e model.Event) error
 
@@ -16,4 +24,6 @@ type DataProvider interface {
 	GetEventsCoveringTimerange(start, end time.Time) ([]model.Event, error)
 
 	// need something here for mutability, e.g. constructing an editor...
+
+	CommitState() error
 }
